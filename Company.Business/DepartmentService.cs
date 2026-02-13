a6a03e1cc0dfd7c6eb0dbc16930cdcb253965e05
Shady Mahmoud <120749934+Shady129@users.noreply.github.com>
2026-02-13 06:02:47 -0800

    Add files via upload

---
+++ Company.Business/DepartmentService.cs
@@ -0,0 +1,76 @@
+﻿using Company.DataAccess;
+
+namespace Company.Business
+{
+    public class DepartmentService
+    {
+
+
+        private DepartmentDAL _departmentDAL;
+
+
+        public DepartmentService()
+        {
+
+            _departmentDAL = new DepartmentDAL();
+
+        }
+
+
+
+        public bool AddDepartment(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                return false;
+            name = name.Trim();
+
+            if (_departmentDAL.IsDepartmentExists(name))
+                return false;
+
+            return _departmentDAL.AddDepartment(name);
+        }
+
+
+
+        public List<Department> GetAllDepartments()
+        {
+            return _departmentDAL.GetAllDepartments();
+        }
+
+
+        public Department GetDepartmentById(int id)
+        {
+            return _departmentDAL.GetDepartmentById(id);
+        }
+
+
+
+
+        public bool UpdateDepartment(int id, string name)
+        {
+            if (string.IsNullOrEmpty(name))
+                return false;
+
+            if (name.Trim().Length < 3)
+                return false;
+
+            if (_departmentDAL.GetDepartmentById(id) == null)
+                return false;
+
+            return _departmentDAL.UpdateDepartment(id, name);
+
+        }
+
+
+ 
+        public bool DeleteDepartment(int id)
+        {
+            var dept = _departmentDAL.GetDepartmentById(id);
+
+            if (dept == null)
+                return false;
+
+            return _departmentDAL.DeleteDepartment(id);
+        }
+    }
+}
---
+++ Company.Business/EmployeeService.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Company.DataAccess;
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Business
+{
+    public class EmployeeService
+    {
+
+        private EmployeeDAL _employeeDAL;
+
+        private DepartmentDAL _departmentDAL;
+
+        public EmployeeService()
+        {
+            _employeeDAL = new EmployeeDAL();
+            _departmentDAL = new DepartmentDAL();
+        }
+
+
+
+        public bool AddEmployee(string name, string email, decimal salary, DateTime hireDate, bool isActive, int departmentId)
+        {
+
+            if(string.IsNullOrWhiteSpace(name))
+                return false;
+
+            name = name.Trim();
+
+            if (name.Trim().Length < 3)
+                return false;
+
+            if(salary <= 0)
+                return false;
+
+            if(_departmentDAL.GetDepartmentById(departmentId) == null)
+                return false;
+
+            return _employeeDAL.AddEmployee(name, email, salary, hireDate, isActive, departmentId);
+        }
+
+
+
+
+
+        public List<Employee>GetAllEmployees()
+        {
+
+            return _employeeDAL.GetEmployees();
+        }
+
+
+        public Employee GetEmployee(int id)
+        {
+            return _employeeDAL.GetEmployee(id);
+        }
+
+
+        public bool UpdateEmployee(int id, string name, string email, decimal salary, int departmentId)
+        {
+
+            if (string.IsNullOrEmpty(name))
+                return false;
+
+            if (name.Trim().Length < 3)
+                return false;
+
+            if (string.IsNullOrWhiteSpace(email))
+                return false;
+
+            if (salary <= 0)
+                return false;
+
+            if (_employeeDAL.GetEmployee(id) == null)
+                return false;
+
+            if (_departmentDAL.GetDepartmentById(departmentId) == null)
+                return false;
+
+            return _employeeDAL.UpdateEmployee(id, name, email, salary, departmentId);
+        }
+
+
+
+        public bool DeleteEmployee(int id)
+        {
+            return _employeeDAL.Delete(id);
+        }
+
+
+
+
+
+    }
+}
